5262bef21746e90d88d0f12385559e520dd0de7e
sahbia <ouaja.sahbia@gmail.com>
2017-09-08 11:27:29 +0200

    add produit file

---
+++ produit.php
@@ -0,0 +1,43 @@
+<?php         
+                  $produit = [['image_src'=>'img/sac1.jpg',
+                                		  'Nom' => 'produit_1',
+
+                                		  'Color' => 'bleu',
+                                 		  'Prix' => '6000',
+                                		  'Description' => 'produit laitier'],
+                                 
+                                   ['image_src'=>'img/sac2.jpg',
+                                		  'Nom' => 'produit_2',
+
+                                		  'Color' => 'rouge',
+                                 		  'Prix' => '5000',
+                                		   'Description' => 'produit menagére'],
+                                    ['image_src'=>'img/sac3.jpg',
+                                	 'Nom' => 'produit_3',
+
+                                	  'Color' => 'vert',
+                                 	 'Prix' => '7000',
+                                    'Description' => 'prduit_beaute'],
+
+                                    ['image_src'=>'img/sac4.jpg',
+                                	 'Nom' => 'produit_6',
+
+                                	  'Color' => 'vert',
+                                 	 'Prix' => '7000',
+                                    'Description' => 'prduit_beaute'],
+                                    ['image_src'=>'img/sac5.jpg',
+                                	 'Nom' => 'produit_4',
+
+                                	  'Color' => 'vert',
+                                 	 'Prix' => '7000',
+                                    'Description' => 'prduit_beaute'],
+
+                                    ['image_src'=>'img/sac6.jpg',
+                                	 'Nom' => 'produit_5',
+
+                                	  'Color' => 'vert',
+                                 	 'Prix' => '7000',
+                                    'Description' => 'prduit_beaute']
+                                    
+                                ];
+ ?>
