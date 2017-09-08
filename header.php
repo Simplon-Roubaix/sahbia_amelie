b71fcc09b6d73a3d6f145fbb445a9ea6419c88b2
Amelie <a.pioger@outlook.fr>
2017-09-08 15:37:58 +0200

    second commit header

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <!-- Place favicon.ico in the root directory -->
 <!-- Google fonts header  -->
-<link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Sacramento" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet">
 
 <!-- Google fonts header  -->
         <link rel="stylesheet" href="css/normalize.css">
@@ -19,17 +19,23 @@
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
     <body>
+      <!-- <div class="container_fluid"> -->
 
-<div id="header">
+       <div class="row entete">
 
-  <div id="logo">
-    <div class="roundedImage">
-  <img class="activator" src="img/caddy3.jpeg"/>
-</div>
-  </div>
-<div id="marque">
-  <p> LA PETITE BOUTIQUE</p>
+            <div id="marque" class="col-12 col-md-12 col-lg-6">
+              <p> La Petite Boutique.com</p>
 
-</div>
-</div>
-<!--header-->
+            </div>
+
+            <div class="form-group col-12 col-md-12 col-lg-6 ">
+                <div class="input-group input-group-sm icon-addon addon-sm">
+                  <input type="text" placeholder="Texte" name="" id="schbox" class="form-control input-sm">
+                  <i class="icon icon-search"></i>
+                  <span class="input-group-btn">
+                    <button type="submit" class="btn btn-inverse">Rechercher</button>
+                  </span>
+                </div>
+              </div>
+            </div>
+<!-- </div> -->
