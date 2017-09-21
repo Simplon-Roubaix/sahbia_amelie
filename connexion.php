266a44d3e0298a2cc74340bd2f5850a8704238fa
sahbia <ouaja.sahbia@gmail.com>
2017-09-21 06:26:13 +0200

    add deconnexion page

---
+++ connexion.php
@@ -0,0 +1,13 @@
+<?php include 'header.php';?>
+<div id="administrateur" class="text-center">
+	 <form action="administrateur.php" method="post" accept-charset="utf-8">
+         User name:
+         <input type="text" name="user">
+         User password:
+
+        <input type="password" name="psw">
+	    <input type="submit" value="se connecte" />
+		
+	 </form>
+	</div>
+    <hr>
